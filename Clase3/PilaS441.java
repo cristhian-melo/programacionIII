28c1513501df1805c039686dc89db64dd1e88964
cristhian <craigoza@estudiante.uniajc.edu.co>
2025-09-16 17:24:08 +0000

    ULTIMO AVANCE

---
+++ Clase3/PilaS441.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+public class PilaS441 {
+    public static void main(String[] args) {
+        
+        //Creación de una pila de números enteros
+        Stack<Integer> pila = new Stack<>();
+
+        //Verificar si la pila está vacía
+        System.out.println(pila.empty()); //true
+
+        //Insertar elementos en la pila
+        pila.push(8);
+        pila.push(4);
+        pila.push(9);
+        pila.push(10);
+        pila.push(6);
+
+        //Imprimir la pila
+        System.out.println(pila);
+
+        //Mostrar el elemento que está en el tope de la pila sin removerlo
+        System.out.println("Tope de la pila: - PilaS441.java:23" + pila.peek());
+
+        //Remover el elemento que se encuentra en el tope de la pila
+        System.out.println("Elemento removido del tope de la pila: - PilaS441.java:26" + pila.pop());
+
+        //Imprimir la pila
+        System.out.println(pila);
+
+        //Buscar y mostrar la posición del elemento dentro de la pila
+        System.out.println(pila.search(10));
+        System.out.println(pila.search(9));
+        System.out.println(pila.search(4));
+        System.out.println(pila.search(8));
+        System.out.println(pila.search(50));
+
+        //Verificar si la pila está vacía
+        System.out.println(pila.empty()); //false
+
+    }
+}
